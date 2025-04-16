30d89dabe9fe31ce5c4745e298f797d2026137b0
danieldjupvik <daniel_22@icloud.com>
2025-04-16 17:22:06 +0200

    Refactor ESLint configuration to extend recommended settings for React and TypeScript; update Node.js version requirement to 22.x in package.json and README; upgrade various dependencies in package.json and package-lock.json for improved functionality; remove unused vitest configuration and proxy setup files; delete public/index.html as part of project restructuring.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,28 @@
 module.exports = {
-  extends: ['react-app'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
-    'no-unused-vars': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
