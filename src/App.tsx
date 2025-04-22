5d69cf9ad01335546d661eb189395c950cf27ca1
danieldjupvik <daniel_22@icloud.com>
2025-04-22 09:51:21 +0200

    Remove customer-related API endpoints and associated context from the project, including the Customer Portal, authentication, and subscription management components; streamline the codebase by deleting unused files and dependencies, enhancing maintainability and reducing complexity.

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { HeroBackground } from './components/HeroBackground';
 import AI from './pages/AI';
 import AIPrivacyPolicy from './pages/AIPrivacyPolicy';
 import AITermsOfServicePage from './pages/AITermsOfServicePage';
-import CustomerPortal from './pages/customer-portal';
-import { CustomerProvider } from './pages/customer-portal/contexts/CustomerContext';
 import Home from './pages/Home';
 import MovieWatcht from './pages/MovieWatcht';
 import Privacy from './pages/Privacy';
@@ -14,22 +12,19 @@ function App() {
   return (
     <div className='App'>
       <HeroBackground />
-      <CustomerProvider>
-        <Router>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/privacy' element={<Privacy />} />
-            <Route path='/moviewatcht' element={<MovieWatcht />} />
-            <Route path='/ai' element={<AI />} />
-            <Route path='/ai/privacy-policy' element={<AIPrivacyPolicy />} />
-            <Route
-              path='/ai/terms-of-service'
-              element={<AITermsOfServicePage />}
-            />
-            <Route path='/customer-portal' element={<CustomerPortal />} />
-          </Routes>
-        </Router>
-      </CustomerProvider>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/privacy' element={<Privacy />} />
+          <Route path='/moviewatcht' element={<MovieWatcht />} />
+          <Route path='/ai' element={<AI />} />
+          <Route path='/ai/privacy-policy' element={<AIPrivacyPolicy />} />
+          <Route
+            path='/ai/terms-of-service'
+            element={<AITermsOfServicePage />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
